ec774ee58e6767a95f352c1174a14d346572a4ac
Iván Vargas O <ivanvargas.va@gmail.com>
2025-10-01 23:55:46 -0500

    feat: begin Startup-app

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './css/normalize.css';
+import './css/variables.css'
+import './css/estilos.css';
+import './css/header.css';
+import './css/hero.css';
+import './css/caracteristicas.css';
+import './css/precios.css';
+import './css/footer.css';
