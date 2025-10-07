53211ec40b00d6370b8dae3fbc64edb64c131275
Iván Vargas O <ivanvargas.va@gmail.com>
2025-10-07 00:40:36 -0500

    feat: Index slideshow works

--- src/js/slideshow.js
+++ src/js/slideshow.js
@@ -2,25 +2,38 @@ const slideshow = document.getElementById('slideshow');
 const slides = document.getElementById('slides');
 const indicadores = document.getElementById('indicadores');
 const btnCambio = document.getElementById('cambiar');
+let slideActual = 1
 
 const cambiarSlide = () => {
     const primerSlide = slides.children[0];
     const anchoSlide = primerSlide.offsetWidth;
 
-    // console.log(anchoSlide);
     slides.style.transform = `translatex(-${anchoSlide}px)`;
     slides.style.transition = `ease-in-out 0.5s all`;
     
-    // Al ejecutar y trasladarse, en 2000ms agrega el primerSlide al final, dejando ver la animación y darle nueva posición 
     setTimeout(()=>{
         slides.appendChild(primerSlide);
         slides.style.transition = `none`;
         slides.style.transform = `translatex(0px)`;
-
+        
     } , 2000);
+    // Al ejecutar y trasladarse, en 2000ms agrega el primerSlide al final, dejando ver la animación y darle nueva posición 
+
+    if(slideActual < slides.children.length) {
+        slideActual++;
+    }
+    else {
+        slideActual = 1;
+    }
+    indicadores.querySelector('.active').classList.remove('active');
+
+    indicadores.children[slideActual -1].classList.add('active');
+    
+
 
 }
 
 setInterval(()=>{
-    cambiarSlide();
+    cambiarSlide(); 
 } , 5000);
+// * Ejecuta función cada 5seg
